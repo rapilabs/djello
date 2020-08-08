2621cc72e2b20ef72ee4fbd173f640b126652d4a
David Sanders <shangxiao>
2020-08-09 03:35:36 +1000

    style: Run Black

--- kanban/models.py
+++ kanban/models.py
@@ -10,26 +10,28 @@ class Board(models.Model):
 
 
 class Column(models.Model):
-    board = models.ForeignKey('Board', related_name='columns', on_delete=models.CASCADE)
+    board = models.ForeignKey("Board", related_name="columns", on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
 
     class Meta:
-        ordering = ['id']
+        ordering = ["id"]
 
     def __str__(self):
-        return '{} - {}'.format(self.board.name, self.title)
+        return "{} - {}".format(self.board.name, self.title)
 
 
 class Card(models.Model):
-    column = models.ForeignKey('Column', related_name='cards', on_delete=models.CASCADE)
+    column = models.ForeignKey("Column", related_name="cards", on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.SlugField(allow_unicode=True)
 
     class Meta:
-        ordering = ['id']
+        ordering = ["id"]
 
     def __str__(self):
-        return '{} - {} - {}'.format(self.column.board.name, self.column.title, self.title)
+        return "{} - {} - {}".format(
+            self.column.board.name, self.column.title, self.title
+        )
 
     def save(self, *args, **kwargs):
         if not self.slug:
--- kanban/urls.py
+++ kanban/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from .views import drop, index, new_card, view_card
 
 urlpatterns = [
-    url(r'^new-card/$', new_card),
-    url(r'^cards/(?P<card_id>\d+)/(?P<card_slug>[\w-]+)/$', view_card),
-    url(r'^drop/$', drop),
-    url(r'^$', index),
+    url(r"^new-card/$", new_card),
+    url(r"^cards/(?P<card_id>\d+)/(?P<card_slug>[\w-]+)/$", view_card),
+    url(r"^drop/$", drop),
+    url(r"^$", index),
 ]
